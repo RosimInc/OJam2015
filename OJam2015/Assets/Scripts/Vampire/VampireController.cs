b2f8735654cbb39dec864a2639272c409a91b0ba
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-27 03:26:48 -0400

    - Finished most of the vampire's animations

--- OJam2015/Assets/Scripts/Vampire/VampireController.cs
+++ OJam2015/Assets/Scripts/Vampire/VampireController.cs
@@ -2,25 +2,41 @@
 using System.Collections;
 using InputHandler;
 
-public class VampireController : MonoBehaviour {
-
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(VampireAnimator))]
+public class VampireController : MonoBehaviour
+{
     public float Speed;
 
+    private Rigidbody2D _rigidBody2D;
+    private VampireAnimator _vampireAnimator;
+
+    private float _xAxisValue;
+
+    void Awake()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _vampireAnimator = GetComponent<VampireAnimator>();
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         InputManager.Instance.PushActiveContext("Gameplay");
         InputManager.Instance.AddCallback(0, HandleVampireActions);
 	}
+
+    void FixedUpdate()
+    {
+        _rigidBody2D.velocity = new Vector2(_xAxisValue * Speed, _rigidBody2D.velocity.y);
+    }
 
     private void HandleVampireActions(MappedInput input)
     {
         if (input.Ranges.ContainsKey("MoveHorizontalVampire"))
         {
-            float range = input.Ranges["MoveHorizontalVampire"];
-
-            transform.position = new Vector3(transform.position.x + Time.deltaTime * range * Speed, 0f, 0f);
+            _xAxisValue = input.Ranges["MoveHorizontalVampire"];
+            _vampireAnimator.Move(_xAxisValue);
         }
     }
-
-
 }
