13669a294f77f40719be349b58c22d3804701a78
RosimInc <rosim_inc@hotmail.com>
2015-06-27 00:16:22 -0400

    Created the bat light and its testbed
    Signed-off-by: RosimInc <rosim_inc@hotmail.com>

---
+++ OJam2015/Assets/Scripts/Game/BatLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatLight : MonoBehaviour {
+
+	public GameObject batty;
+	private Vector3 startPosDif;
+
+	// Use this for initialization
+	void Start () {
+		startPosDif = -batty.transform.position + this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = batty.transform.position + startPosDif;
+	}
+}
