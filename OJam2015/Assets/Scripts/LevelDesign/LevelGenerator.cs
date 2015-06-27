9f4f4c0cbb8140f47f784290341b9f176860ceed
RosimInc <rosim_inc@hotmail.com>
2015-06-27 17:41:25 -0400

    Reorganized crappy code structure
    Signed-off-by: RosimInc <rosim_inc@hotmail.com>

---
+++ OJam2015/Assets/Scripts/LevelDesign/LevelGenerator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject platformPrefab;
+
+	void Awake()
+	{
+		Generator gen = Generator.Instance;
+		gen.platform = platformPrefab;
+		gen.levelHolder = this.gameObject;
+	}
+}
