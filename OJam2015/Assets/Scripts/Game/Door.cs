fed8c355b9154d3cc582645a44974bc6604929e3
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-27 15:16:31 -0400

    - Added the first pass of the door trigger

---
+++ OJam2015/Assets/Scripts/Game/Door.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : Activatable
+{
+    public float FinalHeight = 15f;
+    public float AnimationDuration = 1f;
+
+    // Only for testing without the switchs
+    void Update()
+    {
+        if (Time.time >= 1f)
+        {
+            Activate();
+        }
+    }
+
+    protected override void ActivateAnimation()
+    {
+        StartCoroutine(SlideDoor());
+    }
+
+    private IEnumerator SlideDoor()
+    {
+        float ratio = 0f;
+
+        Vector3 initialPosition = transform.localPosition;
+        Vector3 finalPosition = new Vector3(transform.localPosition.x, FinalHeight, transform.localPosition.z);
+
+        while (ratio < 1f)
+        {
+            ratio += Time.deltaTime / AnimationDuration;
+
+            transform.localPosition = Vector3.Lerp(initialPosition, finalPosition, Mathf.Pow(ratio, 5));
+
+            Debug.Log(transform.localPosition.y);
+
+            yield return null;
+        }
+    }
+}
