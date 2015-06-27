c573f5c0fff3603b3dfa4c98e240c45063baaeff
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-27 17:43:35 -0400

    - First pass of the waterfall

--- OJam2015/Assets/Scripts/Game/Activatable.cs
+++ OJam2015/Assets/Scripts/Game/Activatable.cs
@@ -1,32 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(AudioSource))]
 public abstract class Activatable : MonoBehaviour
 {
-    protected AudioSource _activationSound;
-
-    private bool _activated = false;
-
-    void Awake()
-    {
-        _activationSound = GetComponent<AudioSource>();
-    }
-
-    protected abstract void ActivateAnimation();
-
-    public void Activate()
-    {
-        if (!_activated)
-        {
-            _activated = true;
-
-            if (!_activationSound.isPlaying)
-            {
-                _activationSound.Play();
-            }
-
-            ActivateAnimation();
-        }
-    }
+    public abstract void Activate();
 }
