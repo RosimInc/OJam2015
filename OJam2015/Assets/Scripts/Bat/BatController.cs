65f17716cc2aa3cf0644b77f5b8862f837ac3176
jparent <jeanp.parent@gmail.com>
2015-06-27 12:22:09 -0400

    add suboptimal floaty movement to Bat
    (without perfect angular velocity, I lost too much time on this for now...)

--- OJam2015/Assets/Scripts/Bat/BatController.cs
+++ OJam2015/Assets/Scripts/Bat/BatController.cs
@@ -4,10 +4,13 @@ using InputHandler;
 
 public class BatController : MonoBehaviour {
 
-    public float MaxSpeed;
-    public float MaxVelocity;
-    private float Speed;
-    private float Velocity;
+    public float maxVelocity;
+    private float minVelocity;
+    private float velocityX = 0f;
+    private float velocityY = 0f;
+    public float velocityFactor;
+    public float minMovementRange;
+    public float minVelocityRange;
 
     private bool isRadarActive = false;
 
@@ -15,37 +18,81 @@ public class BatController : MonoBehaviour {
 	void Start () {
         InputManager.Instance.PushActiveContext("Gameplay");
         InputManager.Instance.AddCallback(0, HandleBatActions);
+
+        minVelocity = -1 * maxVelocity;
 	}
 
     private void HandleBatActions(MappedInput input) {
 
-        if (input.Ranges.ContainsKey("MoveHorizontal")) {
-            float range = input.Ranges["MoveHorizontal"];
-
-            transform.position = new Vector3(
-                transform.position.x + Time.deltaTime * range * MaxSpeed,
-                transform.position.y,
-                0f
-            );
-        }
-
-        if (input.Ranges.ContainsKey("MoveVertical")) {
-            float range = input.Ranges["MoveVertical"];
-
-            transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y + Time.deltaTime * range * MaxSpeed,
-                0f
-            );
-
-            
-        }
+        Move(input);
 
         
-        
+
+
+        if (input.Actions.Contains("Action")) {
+
+
+
+        }
 
 
         
     }
+
+
+    private void Move(MappedInput input){
+        if (input.Ranges.ContainsKey("MoveHorizontal")) {
+            float rangeX = input.Ranges["MoveHorizontal"];
+
+            if (rangeX >= minMovementRange || rangeX <= -1 * minMovementRange) {
+                
+                velocityX = Mathf.Clamp(velocityX + velocityFactor * Time.deltaTime * rangeX, minVelocity, maxVelocity);
+
+            }
+
+            // if the joystick is in the deadzone, slow down the character
+            else {
+                if (velocityX > 0) {
+                    velocityX = Mathf.Clamp(velocityX - velocityFactor * 0.5f * Time.deltaTime, 0f, maxVelocity);
+                }
+                else {
+                    velocityX = Mathf.Clamp(velocityX + velocityFactor * 0.5f * Time.deltaTime, minVelocity, 0f);
+                }
+
+            }
+
+            
+        }
+
+        if (input.Ranges.ContainsKey("MoveVertical")) {
+            float rangeY = input.Ranges["MoveVertical"];
+
+            if (rangeY >= minMovementRange || rangeY <= -1 * minMovementRange) {
+
+                velocityY = Mathf.Clamp(velocityY + velocityFactor * Time.deltaTime * rangeY, minVelocity, maxVelocity);
+
+            }
+            // if the joystick is in the deadzone, slow down the character
+            else {
+
+                if (velocityY > 0) {
+
+                    velocityY = Mathf.Clamp(velocityY - velocityFactor *0.5f  * Time.deltaTime, 0f, maxVelocity);
+                }
+                else {
+                    velocityY = Mathf.Clamp(velocityY + velocityFactor * 0.5f * Time.deltaTime, minVelocity, 0f);
+                }
+            }
+
+        }
+
+        transform.position = new Vector3(
+            transform.position.x + velocityX,
+            transform.position.y + velocityY,
+            0f
+        );
+    }
 	
+
+
 }
