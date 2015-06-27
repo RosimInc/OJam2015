31b89da70ce9813c4c2183d5383d158b97e8a3f2
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-27 05:16:11 -0400

    - The basic functionality of the vampire boy is done!

--- OJam2015/Assets/Scripts/Vampire/VampireController.cs
+++ OJam2015/Assets/Scripts/Vampire/VampireController.cs
@@ -2,21 +2,19 @@
 using System.Collections;
 using InputHandler;
 
-[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(VampireAnimator))]
+[RequireComponent(typeof(Vampire))]
 public class VampireController : MonoBehaviour
 {
-    public float Speed;
-
-    private Rigidbody2D _rigidBody2D;
     private VampireAnimator _vampireAnimator;
+    private Vampire _vampire;
 
-    private float _xAxisValue;
+    private bool _jumpAlreadyPressed = false;
 
     void Awake()
     {
-        _rigidBody2D = GetComponent<Rigidbody2D>();
         _vampireAnimator = GetComponent<VampireAnimator>();
+        _vampire = GetComponent<Vampire>();
     }
 
 	// Use this for initialization
@@ -26,17 +24,28 @@ public class VampireController : MonoBehaviour
         InputManager.Instance.AddCallback(0, HandleVampireActions);
 	}
 
-    void FixedUpdate()
-    {
-        _rigidBody2D.velocity = new Vector2(_xAxisValue * Speed, _rigidBody2D.velocity.y);
-    }
-
     private void HandleVampireActions(MappedInput input)
     {
-        if (input.Ranges.ContainsKey("MoveHorizontalVampire"))
+        if (input.Ranges.ContainsKey("MoveHorizontal"))
         {
-            _xAxisValue = input.Ranges["MoveHorizontalVampire"];
-            _vampireAnimator.Move(_xAxisValue);
+            float xAxisValue = input.Ranges["MoveHorizontal"];
+
+            _vampireAnimator.Move(xAxisValue);
+            _vampire.Move(xAxisValue);
         }
+
+        bool jumpPressed = input.States.Contains("Jump");
+        bool isGrounded = _vampire.IsGrounded();
+
+        if (jumpPressed && !_jumpAlreadyPressed && isGrounded)
+        {
+            _vampire.StartJumping();
+        }
+        else if (!jumpPressed)
+        {
+            _vampire.StopJumping();
+        }
+        
+        _jumpAlreadyPressed = jumpPressed;
     }
 }
