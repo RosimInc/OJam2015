e854215700ee07f789a75c7c94b2ca8639f53b20
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-28 12:48:59 -0400

    - Achievementssssssssssssss

---
+++ OJam2015/Assets/Scripts/Menus/AchievementsMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using MenusHandler;
+using InputHandler;
+
+public class AchievementsMenu : Menu
+{
+    public GameObject[] Bats;
+    public GameObject[] LockIcons;
+
+    public override void Open()
+    {
+        base.Open();
+
+        BrainCloudManager.Instance.GetUnlockedAchievements(achievementsCallback);
+    }
+
+    private void achievementsCallback(bool[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Bats[i].SetActive(array[i]);
+            LockIcons[i].SetActive(!array[i]);
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        MenusManager.Instance.ShowMenu("MainMenu");
+    }
+}
