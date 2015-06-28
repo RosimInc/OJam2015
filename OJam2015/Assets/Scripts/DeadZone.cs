0deec44d83c9c23ec5b8399f609158e793dc6452
Patrice Vignola <vignola.patrice@gmail.com>
2015-06-28 13:09:37 -0400

    - Added the death from falling condition

---
+++ OJam2015/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DeadZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Vampire")
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.LoseFromFall();
+            }
+        }
+    }
+}
